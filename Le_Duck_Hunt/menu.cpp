a18c550e3f13c91484a69952fa91a6e23e1a8539
minestro <jacquot.alexandre97@orange.fr>
2015-11-10 08:47:03 +0100

    test

---
+++ Le_Duck_Hunt/menu.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <sstream>
+#include "header.h"
+
+using namespace std;
+
+void showMenu(int &modeMenu, int &modeJeu)
+{
+
+}
+
+bool testHover(int sx, int sy, Sprite bouton)
+{
+    return((sx < bouton.position.x+bouton.lecture.w)&&(sx > bouton.position.x)&&(sy > bouton.position.y)&&(sy < bouton.position.y+bouton.lecture.h));
+}
